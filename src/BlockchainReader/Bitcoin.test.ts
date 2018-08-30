4a91459e03a6a4751baa1fbe2f39d28056725936
lautarodragan <lautaro.dragan@gmail.com>
2018-08-30 00:13:28 -0300

    add test data

--- src/BlockchainReader/Bitcoin.test.ts
+++ src/BlockchainReader/Bitcoin.test.ts
@@ -4,11 +4,13 @@ import { PREFIX_POET, PREFIX_BARD } from 'Helpers/Bitcoin'
 
 import { blockToPoetAnchors } from './Bitcoin'
 
+import * as TestBlock from './TestData/block-00000000000151360aad32397ff1cf7dd303bed163b0ef425e71a53ccdec7312.json'
+
 describe('Bitcoin', async (should: any) => {
   const { assert } = should('')
 
   {
-
+    console.log('block', TestBlock)
   }
 
 })
